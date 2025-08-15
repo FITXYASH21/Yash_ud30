cfd193c5b3fb0855bebcfe12a4352dac608ff632
FITXYASH21 <profeshzone@gmail.com>
2025-08-16 05:07:19 +0530

    Delete assets directory

--- assets/assets/js/faq.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll('.faq .q').forEach((el)=>{
-  el.addEventListener('click',()=>{
-    const a = el.nextElementSibling;
-    if(!a) return;
-    a.style.display = a.style.display === 'block' ? 'none' : 'block';
-  });
-});
